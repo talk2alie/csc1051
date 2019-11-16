afb751e261c04b37f54dabded2bbe5fb47e0c589
Mohamed Pussah <talk2alie@outlook.com>
2019-11-16 11:24:09 -0500

    Add vowels and consonants

---
+++ November13/VowelConsonantOrDigit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * VowelConsonantOrDigit
+ */
+public class VowelConsonantOrDigit {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner("hi there");
+    }
+}
