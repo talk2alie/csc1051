9eaa91131f400a8c99283399ada3249c95fe7ff0
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2019-11-22 17:12:43 -0500

    Add description to methods and stub out questions

--- FinalProject/ShoesByLisa.java
+++ FinalProject/ShoesByLisa.java
@@ -71,13 +71,30 @@ public class ShoesByLisa {
         System.out.println();
     }
 
+    /**
+     * Given a shoeId and a quantity, will make a sale and print a reciept
+     * @param shoeId The index that identifies either the name, price or quantity of a shoe
+     * @param shoeQuantity The number of shoes the buyer wants to buy
+     */
     private static void makeSale(int shoeId, int shoeQuantity) {
-        shoesInStockQuantities[shoeId] -= shoeQuantity;
-        double subTotal = shoesInStockPrices[shoeId] * shoeQuantity;
+        // TODO: Complete this method
 
-        printReciept(shoeId, shoeQuantity, subTotal);
+        // To make a sale, do the following
+        // 1. Subtract the value stored in the shoeQuantity parameter
+        //    from the value located at the index stored in the shoeId 
+        //    parameter, in the shoesInStockQuantities array
+        // 2. Print the user's reciept. Ensure that the reciept includes
+        //    sales tax if you do not want to get in trouble with the IRS
+        // NOTE: I have written a printReciept method for you below. Use it
+        //       if you want
     }
 
+    /**
+     * Given some information, will calculate the sales tax and print a reciept
+     * @param shoeId The index that identifies the name, price or quantity of a shoe
+     * @param shoeQuantity The number of shoes the buyer has purchased
+     * @param subTotal The product of multiplying the number of shoes and the unit price of the shoe
+     */
     private static void printReciept(int shoeId, int shoeQuantity, double subTotal) {
         final double TAX_RATE = 0.06;
         double total = subTotal + (subTotal * TAX_RATE);
@@ -100,6 +117,11 @@ public class ShoesByLisa {
         System.out.println("---------------------------------------------");
     }
 
+    /**
+     * Prints a message that indicates either we are out of a shoe or the remaining number of a given 
+     * shoe we have
+     * @param shoeId The index that identifies the name, price or quantity of a shoe
+     */
     private static void printLimitedQuantityMessageFor(int shoeId) {
         int availableQuantity = shoesInStockQuantities[shoeId];
         String shoeName = shoesInStock[shoeId];
@@ -112,15 +134,35 @@ public class ShoesByLisa {
         System.out.println("Sorry, we only have " + availableQuantity + " of the " + shoeName + "s left.");
     }
 
+    /**
+     * Returns a value that indicates whether we have the number of shoes the buyer wants
+     * @param shoeId The index that identifies the name, price or quantity of a shoe
+     * @param requestedQuantity The number of shoes the buyer wants to buy
+     * @return True if we have the number of shoes the buyer wants; false, otherwise
+     */
     private static boolean weHaveRequetedQuantityInStock(int shoeId, int requestedQuantity) {
-        return shoesInStockQuantities[shoeId] >= requestedQuantity;
+        // TODO: Complete this method
+
+        // Write code here that determins whether we have the number of shoes the user wants.
+        // To accomplish that, do the following
+        // Look in the shoesInStockQuantities array for the value located at the index passed 
+        // in the shoeId variable and check whether that value is larger than or the same 
+        // as the value in the requestedQuantity parameter
     }
 
+    /**
+     * Prints a message that indicates the buyer has requested an invalid number of shoes
+     */
     private static void printShoeQuantityErrorMessage() {
         System.out.println();
         System.out.println("The quantity you entered is invalid. Please enter a positive whole number.");
     }
 
+    /**
+     * Requests the quantity of shoes from the buyer recursively and returns it
+     * @param shoeName The name of the shoe the buyer wants to buy
+     * @return The number of shoes the buyer wants to buy
+     */
     private static int getShoeQuantityFromBuyer(String shoeName) {
         System.out.println();
         try {
@@ -131,10 +173,10 @@ public class ShoesByLisa {
             if(shoeQuantity < 1) {
                 printShoeQuantityErrorMessage();
 
-            // This is called recursion; calling a method inside itself
-            // The net effect of this is similar to using a loop to repeatedly 
-            // ask the buyer for a valid shoe quantity
-            // We will learn about it in 1052
+                // This is called recursion; calling a method inside itself
+                // The net effect of this is similar to using a loop to repeatedly 
+                // ask the buyer for a valid shoe quantity
+                // We will learn about it in 1052
                 shoeQuantity = getShoeQuantityFromBuyer(shoeName);
             }
 
@@ -145,27 +187,44 @@ public class ShoesByLisa {
         }
     }
 
+    /**
+     * Gets a value that indicates whether we have a given shoe in stock
+     * @param shoeId The index that identifies the shoe the buyer wants
+     * @return True if we have the shoe; false, otherwise
+     */
     private static boolean isInStock(int shoeId) {
-        return shoesInStockQuantities[shoeId] > 0;
+        // Write code here that returns true if we have at least of the given shoes
+        // Note that shoeId is an index of a shoe in the arrays at the top of this file
     }
 
+    /**
+     * Prints a message that indicates that we are of stock for the given shoe
+     * @param shoeName The name of the shoe we no longer have
+     */
     private static void printShoeOutOfStockMessageFor(String shoeName) {
         System.out.println();
         System.out.println("Sorry, we are all out of " + shoeName + ".");
         System.out.println("Please come again later.");
     }
 
+    /**
+     * Locates a shoe with a given name and returns it Id (index in the shoesInStock array)
+     * @param shoeName The name of the shoe whose Id to find
+     * @return The index of the shoe in the shoesInStock array
+     */
     private static int findShoeIdBy(String shoeName) {
-
-        for (int index = 0; index < shoesInStock.length; ++index) {
-            if (shoesInStock[index].equalsIgnoreCase(shoeName)) {
-                return index;
-            }
-        }
-
-        return -1;
+        // Write code here that returns the index of the shoe given above
+        // to do that, simply write a loop that goes through shoesInStock 
+        // array and compare each element with the shoeName parameter
+        
+        // If the shoe is found, return the index of the shoe; otherwise, return 
+        // a negative number
     }
 
+    /**
+     * Recursively asks the buyer for a shoe name they want to purchase
+     * @return The name of the shoe the buyer entered on the keyboard
+     */
     private static String getShoeNameFromBuyer() {
         System.out.println();
 
@@ -183,11 +242,17 @@ public class ShoesByLisa {
         return shoeName;
     }
 
+    /**
+     * Prints a message that indicates the buyer has entered an invalid shoe name
+     */
     private static void printShoeNameErrorMessage() {
         System.out.println();
         System.out.println("Please enter a valid shoe name.");
     }
 
+    /**
+     * Prints a message to welcome the buyer to our store
+     */
     private static void printGreeting() {
         System.out.println("Hi, thank you for visiting us today.");
         System.out.println("We have all the latest in shoe designs.");
